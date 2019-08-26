fbb374b9f61cee9877953e40754b144d05ae039d
Jesus Romero <jhromero@tekmexico.com>
2019-08-26 16:50:28 -0700

    Add connect and disconnect event handlers

--- dist/server/server.js
+++ dist/server/server.js
@@ -5,9 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
+const socket_io_1 = __importDefault(require("socket.io"));
+const http_1 = __importDefault(require("http"));
 const app = express_1.default();
 const publicPath = path_1.default.join(__dirname, '../../public');
 const port = process.env.PORT || 3000;
+const server = http_1.default.createServer(app);
+const io = socket_io_1.default(server);
 app.use(express_1.default.static(publicPath));
-app.listen(port, () => console.log(`Listening on port ${port}`));
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+server.listen(port, () => console.log(`Listening on port ${port}`));
 //# sourceMappingURL=server.js.map
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,20 @@
 declare const __dirname;
 import path from 'path';
 import express from 'express';
+import socketIO from 'socket.io';
+import http from 'http';
 
 const app = express();
-const publicPath = path.join(__dirname,'../../public');
+const publicPath = path.join(__dirname, '../../public');
 const port = process.env.PORT || 3000;
-
+const server = http.createServer(app);
+const io = socketIO(server);
 app.use(express.static(publicPath));
-app.listen(port, () => console.log(`Listening on port ${port}`));
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    })
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
