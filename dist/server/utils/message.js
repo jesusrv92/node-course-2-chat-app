1032bd23791f6b67d1989d9db9f3201df4a064d1
Jesus Romero <jhromero@tekmexico.com>
2019-08-28 12:55:24 -0700

    Corrected geolocation api

--- dist/server/utils/message.js
+++ dist/server/utils/message.js
@@ -12,7 +12,7 @@ exports.generateMessage = generateMessage;
 function generateLocationMessage(from, lat, lon) {
     return {
         from,
-        url: `https://www.google.com/maps/@${lat},${lon}z`,
+        url: `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`,
         createdAt: new Date().getTime()
     };
 }
--- src/server/utils/message.ts
+++ src/server/utils/message.ts
@@ -8,7 +8,7 @@ export function generateMessage(from: string, text: string) {
 export function generateLocationMessage(from: string, lat: number, lon: number) {
     return {
         from,
-        url: `https://www.google.com/maps/@${lat},${lon}z`,
+        url: `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`,
         createdAt: new Date().getTime()
     };
 };
